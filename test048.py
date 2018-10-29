5643a8203279ee0c94b936472a819d1ec8f6ffae
wangsong <wshappyday@sina.com>
2018-10-29 16:02:47 +0800

    2018.10.29

---
+++ test048.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from enum import Enum, unique
+
+@unique
+class Weekday(Enum):
+    Sun = 0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+day1 = Weekday.Mon
+print(day1)
+print(Weekday.Tue)
+print(Weekday['Tue'])
+print(Weekday.Tue.value)
+print(day1 == Weekday.Mon)
+print(day1 == Weekday.Tue)
+print(Weekday(1))
+print(day1 == Weekday(1))
+for name, member in Weekday.__members__.items():
+    print(name, '=>', member)
---
+++ test049/test049.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from hello import Hello
+
+h = Hello()
+h.hello()
